6e4c61b8af25dbb3c5f2bf5424b68bc9ec858151
Arifa <arifa@Zedstars-MacBook-Pro.local>
2018-05-18 15:00:36 +0300

    Added Extra functionalties

--- index.js
+++ index.js
@@ -27,28 +27,30 @@ let tasks = [
 
     let data = document.getElementById('table');
     for( let i=0; i<tasks.length;i++){
-    let row = document.createElement("tr");
-    let button = document.createElement("button");
-    let button_text = document.createTextNode("X");
-    let checkbox= document.createElement("input");
-    checkbox.type = 'checkbox';
-    checkbox.className = 'checkbox';
+        let row = document.createElement("tr");
+        row.addEventListener('click',makeEdit,false);
+        let button = document.createElement("button");
+    // let button_text = document.createTextNode("X");
+    // let button_text.className= "";
+        let checkbox= document.createElement("input");
+        checkbox.type = 'checkbox';
+        checkbox.className = 'checkbox';
     //Object.keys(tasks[i])
-    for(let key in tasks[i] ) {
-        if(key == 'id'){
-            break;
-        }
-        let cell = document.createElement("td");
+        for(let key in tasks[i] ) {
+            if(key == 'id'){
+             break;
+            }
+            let cell = document.createElement("td");
         
-        let cellText = document.createTextNode(tasks[i][key]);
-        cell.appendChild(cellText);
-        row.appendChild(cell);
-        button.appendChild(button_text);
-        button.className="deleteTask";
-        button.addEventListener('click',deleteTask,false);
-        row.appendChild(button);
-        checkbox.addEventListener('click', handleChecked);
-        row.appendChild(checkbox);
+            let cellText = document.createTextNode(tasks[i][key]);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        // button.appendChild(button_text);
+            button.className="deleteTask fa fa-close";
+            button.addEventListener('click',deleteTask,false);
+            row.appendChild(button);
+            checkbox.addEventListener('click', handleChecked);
+            row.appendChild(checkbox);
        
     }
     
@@ -60,10 +62,14 @@ const addTask = () =>{
     let newItems= [];
    const name = document.getElementById('nameBox').value;
    const qty = document.getElementById('qtyBox').value;
+   let modal = document.getElementById('myModal');
+   
    if(name === "" || qty === ""){
-    alert("Please Enter correct values!");
-    return;
+    //alert("Please Enter correct values!");
+        modal.style.display = "block";
+        return;
    }
+  
    const id = Math.floor(Math.random() * 100 +5);
 
     console.log("name: ",name, "qty:",qty);
@@ -73,7 +79,7 @@ const addTask = () =>{
     let data = document.getElementById('table');
     let row = document.createElement("tr");
     let button = document.createElement("button");
-    let button_text = document.createTextNode("X");
+    //let button_text = document.createTextNode("X");
     let checkbox= document.createElement("input");
     checkbox.type = 'checkbox';
     checkbox.className = 'checkbox';
@@ -86,10 +92,11 @@ const addTask = () =>{
         let cellText= document.createTextNode(newItems[i]);
         cell.appendChild(cellText);       
         row.appendChild(cell);
+        button.className="deleteTask fa fa-close";
+        button.addEventListener('click',deleteTask,false);
+        // button.className="deleteTask";
+        // button.onclick = deleteTask;
         row.appendChild(button);
-        button.appendChild(button_text);
-        button.className="deleteTask";
-        button.onclick = deleteTask;
         checkbox.addEventListener('click', handleChecked);
         row.appendChild(checkbox);
     }
@@ -98,7 +105,19 @@ const addTask = () =>{
     document.getElementById('nameBox').value="";
     document.getElementById('qtyBox').value="";
 }
+const close = () => {
+    let modal = document.getElementById('myModal');
+    console.log('modal', modal)
+    modal.style.display = "none";
+}
 
+const modal = document.getElementById('myModal');
+
+modal.addEventListener("click", function(e){
+    if(e.currentTarget.id === "myModal"){
+        modal.style.display = "none";
+    }
+});
 const deleteTask = (e) =>{
     console.log('delete me');
     console.dir(e.target.parentElement.innerText);
@@ -108,15 +127,15 @@ const deleteTask = (e) =>{
 const handleChecked = (e) => {
     let addrow = e.target.parentElement;
     let tablearea= document.getElementById('DoneTable');
-    let doneTable= document.createElement('table');
+    // let doneTable= document.createElement('table');
 
     addrow.lastChild.remove();
     let num= document.createElement('i');
     num.className="undo fa fa-undo";
     num.addEventListener('click',handleUndo);
     addrow.appendChild(num);
-    doneTable.appendChild(addrow);
-    tablearea.appendChild(doneTable);
+    // doneTable.appendChild(addrow);
+    tablearea.appendChild(addrow);
 
 }
 
@@ -125,6 +144,9 @@ function toggleContents(){
     let table = document.getElementById('DoneTable');
     let font = document.createElement("i");
     let label = document.getElementById('completedList');
+    if(table.childNodes.length===1){
+        return;
+    }
     if (document.getElementById('DoneTable').style.visibility == "visible") {
         document.getElementById('DoneTable').style.visibility = "hidden";
         font.className="arrow fa fa-angle-up";
@@ -151,4 +173,26 @@ function handleUndo(e) {
     addrow.appendChild(checkbox);
     data.appendChild(addrow);
 
+}
+const makeEdit = (e) => {
+    e.target.contentEditable = "true";
+}
+const uncheckAll = () =>{
+    let data = document.getElementById('DoneTable');
+    const new_array = Array.from(data.children);
+    let Add_data = document.getElementById('table');
+    for(let i=0;i<new_array.length;i++){
+        new_array[i].lastChild.remove();
+        let checkbox= document.createElement("input");
+        checkbox.type = 'checkbox';
+        checkbox.className = 'checkbox';
+        checkbox.addEventListener('click', handleChecked);
+        new_array[i].appendChild(checkbox);
+        Add_data.appendChild(new_array[i]);
+    }
+}
+
+const removechecked = () =>{
+    let data = document.getElementById('DoneTable');
+    data.innerHTML="";
 }
